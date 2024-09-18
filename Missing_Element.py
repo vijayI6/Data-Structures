3fce5d45578324903d97ac554f17b822f0ac11a8
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2024-09-18 15:36:34 +0530

    Missing_Element.py

--- Missing_Element.py
+++ Missing_Element.py
@@ -2,9 +2,18 @@
 Rule: (sorted / unsorted) data must be in a sequence and n is greater than len(lst)
 """
 
-n = int(input('Enter length:'))
-lst = list(map(int, input().split()))
-print((n * (n + 1) // 2) - sum(lst), 'is the missing number')
+n = int(input(" Enter length: ")
+lst = list(map(int, input().split(" ")))
+print((n * (n + 1) // 2) - sum(lst), "is the missing number")
+
+
+# 2-Approach 
+size = int(input())
+data = list(map(int, input().split(" ")))
+for i in range(1, size + 1):
+    if i not in data:
+        print(i, "is the missing number")
+
 
 # sample output:
 # n = 6
