1cca15286a4e614ab01ca06b016e513f91ad788b
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2025-02-18 19:54:49 +0530

    Second_Largest_Number.py

--- Arrays/Second_Largest_Number.py
+++ Arrays/Second_Largest_Number.py
@@ -19,12 +19,33 @@ def find2LargestII(arr):
     return res[-2]
 
 
+# Approach-3
+def find2LargestIII(arr):
+    if len(arr) < 2:
+        return 0
+
+    max1 = float("-inf")
+    max2 = float("-inf")
+
+    for ele in arr:
+        if ele > max1:
+            max2 = max1
+            max1 = ele
+        elif ele > max2 and ele != max1:
+            max2 = ele
+
+    return max2 if max2 != float("-inf") else -1
+
+
+
 if __name__ == '__main__':
     lst = [10, 9, 11, 15]
     print(find2LargestI(lst))
     print(find2LargestII(lst))
+    print(find2LargestIII(lst))
 
 
 # Output:
+# 11
 # 11
 # 11
