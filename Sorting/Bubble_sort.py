5bb7c05a6be9f9a5aa9a6ef57bd1a665e846c713
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2025-05-28 12:17:03 +0530

    Bubble_sort.py

--- Sorting/Bubble_sort.py
+++ Sorting/Bubble_sort.py
@@ -1,26 +1,24 @@
 """
-Bubble sort / Sinking sort algorithm in python
+Bubble sort / Sinking sort algorithm in Python
 
-    Best-Case --> O(n)
-    Worst-Case --> O(n*n)
-    Average-Case --> O(n*n)
-    Space Complexity --> O(1)
-    
 Technique: 
 
     1) First we need to find length of the list(N) then we require N-1 passes for sort the given list
+    
     2) In pass_1
             array[0] is compared with array[1] 
-            array[1] is compared with array[2] and so on
-            At the end of the pass_1 largest element in the list is placed at highest index on the list
+            array[1] is compared with array[2] and .......
+            At the end of the pass_1, the largest element in the list is placed at the highest index on the list
+            
     3) In pass_2
             array[0] is compared with array[1] 
-            array[1] is compared with array[2] and so on
-            At the end of the pass_2 second largest element in the list is placed at second highest index on the list
+            array[1] is compared with array[2] and so .......
+            At the end of the pass_2, the second largest element in the list is placed at the second highest index on the list
+            
     4) In pass(N-1)
             array[0] is compared with array[1] 
-            array[1] is compared with array[2] and so on
-            At the end of the pass(N-1) smallest element in the list is placed at first index on the list  
+            array[1] is compared with array[2] and .......
+            At the end of the pass(N-1) smallest element in the list is placed at the first index on the list  
 """
 
 
@@ -28,7 +26,7 @@ def bubble_sort(a):
     n = len(a)
     for i in range(n - 1):  # outerloop for passes (n-1)
         swapped = False
-        for j in range(0, n - i - 1):
+        for j in range(0, n - i - 1): # Inner loop avoids sorted part
             if a[j] > a[j + 1]:  # comparison and swapping
                 swapped = True
                 a[j], a[j + 1] = a[j + 1], a[j]
@@ -36,13 +34,13 @@ def bubble_sort(a):
             break
 
 
-lst = list(map(int, input().split(' ')))
+lst = list(map(int, input().split()))
 bubble_sort(lst)
-print("The Bubble Sort of given list is:")
+print("The Bubble Sort of the given list is:")
 for i in range(len(lst)):
     print(lst[i], end=" ")
 
 # output:
 # 5 8 9 1 10 15 2 4
-# The Bubble Sort of given list is:
+# The Bubble Sort of the given list is:
 # 1 2 4 5 8 9 10 15
