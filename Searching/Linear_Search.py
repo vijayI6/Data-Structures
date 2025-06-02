71b9451e152701cb2d5b03c839c3c3afa8fc82e0
Sai Vijay <142137346+vijayI6@users.noreply.github.com>
2025-06-02 19:39:45 +0530

    Linear_Search.py

--- Searching/Linear_Search.py
+++ Searching/Linear_Search.py
@@ -1,8 +1,7 @@
 """
-Linear Search is also called Sequential Search and it is a simple method that is used to find a particular value in a array.
+Linear Search is also called Sequential Search and it is a simple method that is used to find a particular value in an array.
 It works by comparing a given value with every value in the array one by one in the sequence until a match is found
  
-
 """
 class LinearSearch:
     def __init__(self: object, array: list, value: int) -> None:
@@ -14,8 +13,6 @@ class LinearSearch:
             if self.value == self.array[i]:
                 return i
         return -1
-
-
 
 arr = list(map(int, input().split()))
 val = int(input())
