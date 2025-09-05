5bddc1221b5fd37997ef33858777092cfffeab90
Vijay <142137346+vijayI6@users.noreply.github.com>
2025-09-05 13:13:46 +0530

    Linear_Search.py

--- Searching/Linear_Search.py
+++ Searching/Linear_Search.py
@@ -4,11 +4,11 @@ It works by comparing a given value with every value in the array one by one in 
  
 """
 class LinearSearch:
-    def __init__(self: object, array: list, value: int) -> None:
+    def __init__(self, array, value):
         self.array = array
         self.value = value
 
-    def Searching(self) -> int:
+    def Searching(self):
         for i in range(len(self.array)):
             if self.value == self.array[i]:
                 return i
