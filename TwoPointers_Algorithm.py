098e928434184afa8e5212f40b4d9900c49f15d7
Vijay <142137346+vijayI6@users.noreply.github.com>
2025-12-11 14:39:56 +0530

    Update TwoPointers_Algorithm.py

--- TwoPointers_Algorithm.py
+++ TwoPointers_Algorithm.py
@@ -1,5 +1,5 @@
 """
-Two-Pointer Algorithm: This is a programming method that solves problems by using two pointers (or indices) to navigate through data structures like arrays or lists.
+Two-Pointer Algorithm: This is a programming method that solves problems by using two pointers (or indices) to navigate data structures like arrays or lists.
 
 Syntax:
 
@@ -48,7 +48,7 @@ print(two_sum(arr, target))   # Output:  (4, 6)
 
 
 
-# Ex: To check whether the given data is palindrome or not.
+# Ex: To check whether the given data is a palindrome or not.
 def check_palindrome(var):
     left = 0
     right = len(var) - 1
